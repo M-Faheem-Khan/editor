32aff51860ad5631025e2d054faae884c4ae890f
real_migical_unicorn <faheem5948@gmail.com>
2016-12-22 04:30:53 -0500

    Delete live_preview.js

--- live_preview.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('editor').onkeyup = document.getElementById('editor').onkeypress = function() {
-     document.getElementById('preview').innerHTML = this.value;
-}
-//SAME AS THIS
-//var editor = document.getElementById('editor');
-//editor.onkeyup = editor.onkeypress = function() {document.getElementById('preview').innerHTML = this.value;}​;
